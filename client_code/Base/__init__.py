e0d1e2d21a5c26ec3d241583a0908e6c39cfa966
siuyeung.chow@uqconnect.edu.au <siuyeung.chow@uqconnect.edu.au>
2024-12-02 00:25:48 +0000

    Edited UnTANGLeD, Base and TRIAGE

--- client_code/Base/__init__.py
+++ client_code/Base/__init__.py
@@ -1,6 +1,10 @@
 from ._anvil_designer import BaseTemplate
 from anvil import *
 from ..Home import Home
+from ..TRIAGE import TRIAGE
+from ..CMPortal import CMPortal
+from ..EpiCops import EpiCops
+from ..UnTANGLeD import UnTANGLeD
 
 class Base(BaseTemplate):
     def __init__(self, **properties):
@@ -10,14 +14,11 @@ class Base(BaseTemplate):
         # self.pages_dict = {'Home' : Home()}
         
         self.link_1.text = 'Palpant Group'
-        self.button_1.text = 'TRIAGE'
-        self.button_2.text = 'TRIAGE'
+        self.button_1.text, self.button_2.text, self.button_2.text, self.button_2.text = 'TRIAGE', 'EpiCops', 'CMPortal', 'UnTANGLeD'
         
         # self.content_panel.add_component(self.pages_dict['Home'])
         self.content_panel.add_component(Home())
         
-        
-
     def button_1_click(self, **event_args):
         """This method is called when the button is clicked"""
         self.content_panel.clear()
