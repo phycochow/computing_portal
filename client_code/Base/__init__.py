4b8a9d441c248eb9d771420e37627be6574c013e
siuyeung.chow@uqconnect.edu.au <siuyeung.chow@uqconnect.edu.au>
2024-12-06 05:16:33 +0000

    Edited theme.css and Base; Deleted UnTANGLeD and TRIAGE

--- client_code/Base/__init__.py
+++ client_code/Base/__init__.py
@@ -1,62 +1,60 @@
 from ._anvil_designer import BaseTemplate
 from anvil import *
 from ..Home import Home
-from ..TRIAGE import TRIAGE
 from ..CMPortal import CMPortal
 from ..EpiCops import EpiCops
-from ..UnTANGLeD import UnTANGLeD
 
 class Base(BaseTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
         # Any code you write here will run before the form opens.
-        # self.pages_dict = {'Home' : Home()}
+        # self.pages_dict = {'Home' : Home(), 'Epi'}
 
-        self.navg_buttons = [self.button_1, self.button_2, self.button_3, self.button_4]
+        self.navg_links = [self.link_1]
+        self.side_buttons = [self.CMPortal_button, self.epicops_button, self.home_button]
 
         # self.content_panel.add_component(self.pages_dict['Home'])
-        self.content_panel.add_component(Home())
-        self.title.content = 'Palpant Group'
+        # self.content_panel.add_component(Home())
+        # self.home_button.role = 'tonal-button'
+        # self.title.content = 'Palpant Group'
+        # self.link_1.text = 'hPSCs'
+        self.home_button_click()
         
-    def button_1_click(self, **event_args):
+    def epicops_button_click(self, **event_args):
         """This method is called when the button is clicked"""
-        self.content_panel.clear()
-        self.content_panel.add_component(TRIAGE())
-        for navg_button in self.navg_buttons:
-            navg_button.role = 'default'
-        self.button_1.role = 'tonal-button'
-        self.title.content = 'TRIAGE'
-
-    def button_2_click(self, **event_args):
-        """This method is called when the button is clicked"""
+        # Update content
         self.content_panel.clear()
         self.content_panel.add_component(EpiCops())
-        for navg_button in self.navg_buttons:
-            navg_button.role = 'default'
-        self.button_2.role = 'tonal-button'
+
+        # Update left side bar
+        for side_button in self.side_buttons:
+            side_button.role = 'default'
+        self.epicops_button.role = 'tonal-button'
         self.title.content = 'EpiCops'
+
+        # Update top navigation bar
+        # links_action_dict = {'Introduction': Tools["EpiCops"]['Intro'] }
+        # self.navg_links = [self.link_1]
+        
     
 
-    def button_3_click(self, **event_args):
+    def CMPortal_button_click(self, **event_args):
         """This method is called when the button is clicked"""
         self.content_panel.clear()
         self.content_panel.add_component(CMPortal())
-        for navg_button in self.navg_buttons:
-            navg_button.role = 'default'
-        self.button_3.role = 'tonal-button'
+        for side_button in self.side_buttons:
+            side_button.role = 'default'
+        self.CMPortal_button.role = 'tonal-button'
         self.title.content = 'CMPortal'
-        
-    def button_4_click(self, **event_args):
-        """This method is called when the button is clicked"""
-        self.content_panel.clear()
-        self.content_panel.add_component(UnTANGLeD())
-        for navg_button in self.navg_buttons:
-            navg_button.role = 'default'
-        self.button_4.role = 'tonal-button'
-        self.title.content = 'UnTANGLeD'
 
-    def button_5_click(self, **event_args):
+    def home_button_click(self, **event_args):
         """This method is called when the button is clicked"""
         self.content_panel.clear()
         self.content_panel.add_component(Home())
+        for side_button in self.side_buttons:
+            side_button.role = 'default'
+        self.home_button.role = 'tonal-button'
+        self.title.content = 'Palpant Group'
+
+        self.link_1.text = 'hPSCs'
