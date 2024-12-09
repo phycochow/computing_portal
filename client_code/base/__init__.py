28de5b8c5dbcedfa444780c966c61adaccbb5758
siuyeung.chow@uqconnect.edu.au <siuyeung.chow@uqconnect.edu.au>
2024-12-09 02:26:29 +0000

    Created blood.png and PalpantLogo.jpeg; Edited theme.css, settings, standard-page.html and base; Deleted palpantLogo.png

--- client_code/base/__init__.py
+++ client_code/base/__init__.py
@@ -28,11 +28,11 @@ class base(baseTemplate):
         self.home_button.role = 'tonal-button'
 
         # Update top navigation bar
-        self.splitter_2.visible = False
-        self.link_4.visible = False
+        self.splitter_2.visible = True
         self.link_5.visible = False
         self.link_6.visible = False
         self.link_7.visible = False
+        self.link_8.visible = False
 
     def epicops_button_click(self, **event_args):
         """This method is called when the button is clicked"""
@@ -47,15 +47,15 @@ class base(baseTemplate):
 
         # Update top navigation bar
         self.splitter_2.visible = True
-        self.link_4.visible = True
         self.link_5.visible = True
         self.link_6.visible = True
         self.link_7.visible = True
+        self.link_8.visible = True
         
-        self.link_4.text = "Introduction"
-        self.link_5.text = "Visuals"
-        self.link_6.text = "Predictions"
-        self.link_7.text = "Clustering"
+        self.link_5.text = "Introduction"
+        self.link_6.text = "Visuals"
+        self.link_7.text = "Predictions"
+        self.link_8.text = "Clustering"
     
     def CMPortal_button_click(self, **event_args):
         """This method is called when the button is clicked"""
@@ -67,15 +67,15 @@ class base(baseTemplate):
 
         # Update top navigation bar
         self.splitter_2.visible = True
-        self.link_4.visible = True
         self.link_5.visible = True
         self.link_6.visible = True
         self.link_7.visible = True
+        self.link_8.visible = True
         
-        self.link_4.text = "Introduction"
-        self.link_5.text = "Database"
-        self.link_6.text = "Enrichments"
-        self.link_7.text = "Predictions"
+        self.link_5.text = "Introduction"
+        self.link_6.text = "Database"
+        self.link_7.text = "Enrichments"
+        self.link_8.text = "Predictions"
 
     def drop_down_1_change(self, **event_args):
         """This method is called when an item is selected"""
