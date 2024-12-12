77d624c590b031652cbdaf8bf96c99ac170f6c89
siuyeung.chow@uqconnect.edu.au <siuyeung.chow@uqconnect.edu.au>
2024-12-12 05:53:58 +0000

    Edited EpiCops, EpiCops.Clustering, EpiCops.FunVisuals, EpiCops.ModelTraining and 3 more

--- client_code/EpiCops/FunVisuals/__init__.py
+++ client_code/EpiCops/FunVisuals/__init__.py
@@ -7,5 +7,8 @@ class FunVisuals(FunVisualsTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-
+        
+        fig1 = anvil.server.call('create_plots')
+        self.image_1.source = fig1
+        self.image_2.source = fig1
         # Any code you write here will run before the form opens.
