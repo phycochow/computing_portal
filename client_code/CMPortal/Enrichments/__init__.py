3c7b30bf83f8070a8a216539e43530f47d8d90a1
siuyeung.chow@uqconnect.edu.au <siuyeung.chow@uqconnect.edu.au>
2024-12-18 12:38:03 +0000

    Edited CMPortal.Enrichments and Home

--- client_code/CMPortal/Enrichments/__init__.py
+++ client_code/CMPortal/Enrichments/__init__.py
@@ -14,4 +14,33 @@ class Enrichments(EnrichmentsTemplate):
 
         # Any code you write here will run before the form opens.
         self.column_panel_1.role = "well-style"
+
+
+    def create_tabs(self):
+
+        self.tabs_1.clear()
         
+        self.tabs_lst = ['tab1', 'tab2', 'tab10']
+
+        # Create a dictionary to store tabs and content
+        self.tab_content = {}
+        
+        for tab_name in self.tabs_lst:
+            # Create a button for the tab
+            tab_button = Button(text=tab_name, role="raised-button")
+            tab_button.set_event_handler('click', self.tab_button_click)
+            
+            # Create a Label or content for the tab
+            tab_content = Label(text=f"Content for {tab_name}", visible=False)
+            
+            # Add button and content to the ColumnPanel
+            self.tabs_1.add_component(tab_button)
+            self.tabs_1.add_component(tab_content)
+            
+            # Store the content in the dictionary for toggling
+            self.tab_content[tab_name] = tab_content
+        
+    def tab_button_click(self, sender, **event_args):
+        # Toggle visibility of the corresponding tab content
+        for tab_name, content in self.tab_content.items():
+            content.visible = (sender.text == tab_name)
--- client_code/Home/__init__.py
+++ client_code/Home/__init__.py
@@ -13,18 +13,5 @@ class Home(HomeTemplate):
         self.navg_text = ('', '', '', '', '')
         
         # Any code you write here will run before the form opens.
-        text = self.rich_text_2
-        
-        self.grid_panel_1.row = 'gp-centered'
-        self.grid_panel_1.clear()
-        self.grid_panel_1.add_component(text, row='row_panel', col_xs=0, width_xs=6)
-        self.grid_panel_1.add_component(Image(source='_/theme/PalpantLogo.jpeg',
-                                              display_mode='fill_width', 
-                                              margin=('15px', '0px', '0px', '0px')), # Top, Right, Bottom, Left
-                                        row='row_panel',
-                                        col_xs=8.5,
-                                        width_xs=3.5)
-
-
         
         
